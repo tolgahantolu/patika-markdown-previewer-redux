8df8cab9904e3063ec61fd6afdfae1cf44b20a66
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-04-01 10:07:46 +0300

    fresh start

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "theme-background": "#ffcc38",
+        "theme-input-bg": "#ffdd67",
+        "theme-question-bg": "#ffeeB4",
+        "theme-color": "#4a4a4a",
+      },
+    },
   },
   plugins: [],
 };
-
