c42df3c116f5c63bd03bd17ae8d9f1401bd1d98f
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-04-01 10:11:08 +0300

    redux store config

---
+++ src/store/markdownSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const markdownSlice = createSlice({
+  name: "markdown",
+  initialState: {},
+  reducers: {},
+});
+
+export default markdownSlice.reducer;
